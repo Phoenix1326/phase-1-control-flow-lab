b4beb77335998079a809e31bdce888715c1288bc
Liliana <lilmartinez@ucdavis.edu>
2021-12-07 21:00:08 -0800

    commit tree 867295a8ad656e3c4e48ed451f66ce265e2dc37b

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400)
+    {return 'This one is on me!'}
+  else if (feet > 2000 && feet <=2500)
+    {return 'I will gladly take your thirty bucks.'}
+  else (feet >= 2500)
+    {return 'No can do.'}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ const cityWording = (city === 'NYC') ? ('Ok, sounds good.') : ('No go.');
+ return cityWording
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip)
+  {case 'generous' : 
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.';
+  default:
+    return 'Bye.';
+    break;}
+    tip;
 }
